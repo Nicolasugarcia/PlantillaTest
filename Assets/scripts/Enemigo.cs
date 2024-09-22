607274eacf3df4d322af49521f5f077e9cf5e788
Nicolasugarcia <102266660+Nicolasugarcia@users.noreply.github.com>
2024-09-22 17:05:15 -0300

    Debug
    Correcion de movimiento

--- Assets/scripts/Enemigo.cs
+++ Assets/scripts/Enemigo.cs
@@ -4,45 +4,34 @@ using UnityEngine;
 
 public class Enemigo : MonoBehaviour
 {
-    public float speed = 2f; 
-    public float limite1 = 5f; 
-    public float limite2 = -5f; 
+
+    public float speed = 2f;
+    public float limite1 = 7f;
+    public float limite2 = -7f;
     private Vector3 direccion1 = Vector3.down;
-    private Vector3 direccion2 = Vector3.left;
+   
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         MoverArribaAbajo();
-        MoverIzqDer();
+       
     }
     private void MoverArribaAbajo()
     {
-       
-        transform.Translate(direccion1 * speed * Time.deltaTime);
-        if (transform.position.y <= limite1)
-        {
-            direccion1 = Vector3.up; 
-        }
-        else if (transform.position.y >= limite2)
-        {
-            direccion1 = Vector3.down; 
-        }
-    }
-    private void MoverIzqDer()
-    {
 
-        transform.Translate(direccion2 * speed * Time.deltaTime);
-        if (transform.position.x <= limite1)
+        transform.Translate(direccion1 * speed * Time.deltaTime);
+        if (transform.position.y < limite2)
         {
-            direccion1 = Vector3.right;
+            direccion1 = Vector3.up;
         }
-        else if (transform.position.x >= limite2)
+        else if (transform.position.y > limite1)
         {
-            direccion1 = Vector3.left;
+            direccion1 = Vector3.down;
         }
     }
+   
 }
